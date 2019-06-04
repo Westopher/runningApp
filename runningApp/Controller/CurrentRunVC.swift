8f8a61a83586308d1d5d78a49fbbfdac7b763f04
West Kraemer <kraemerwest@gmail.com>
2019-06-04 15:29:29 -0400

    finish slider ui

---
+++ runningApp/Controller/CurrentRunVC.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentRunVC.swift
+//  runningApp
+//
+//  Created by West Kraemer on 6/4/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
